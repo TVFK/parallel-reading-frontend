b99a1d76aae488f63417e7b938558db337cce7dd
Tavfik <tavfik2408@gmail.com>
2025-09-03 19:39:40 +0300

    fix(dictionary-page): fix filters

--- src/components/dictoinary-page/DictionaryFilters.vue
+++ src/components/dictoinary-page/DictionaryFilters.vue
@@ -2,7 +2,7 @@
   <div class="flex flex-wrap gap-4 items-center">
     <div class="relative w-full md:w-auto">
       <select :value="sortOption" @change="handleChange"
-        class="appearance-none w-full md:w-64 px-4 py-2 pr-10 bg-white border border-gray-300 rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-[#A9A8FD] focus:border-transparent">
+        class="cursor-pointer appearance-none w-full md:w-64 px-4 py-2 pr-10 bg-white border border-gray-300 rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-[#A9A8FD] focus:border-transparent">
         <option value="date">По дате добавления (новые)</option>
         <option value="alphabet">По алфавиту (А-Я)</option>
         <option value="review">По дате повторения</option>
@@ -16,21 +16,12 @@
         </svg>
       </div>
     </div>
-
-    <div class="flex gap-2 flex-wrap">
-      <button v-for="(filter, index) in filters" :key="index" @click="$emit('toggle-filter', filter)"
-        :class="{ 'bg-[#A9A8FD] text-white': activeFilters.includes(filter), 'bg-gray-100 text-gray-700': !activeFilters.includes(filter) }"
-        class="px-4 py-2 rounded-lg transition-colors">
-        {{ filter }}
-      </button>
-    </div>
   </div>
 </template>
 
 <script setup lang="ts">
 const props = defineProps({
   sortOption: { type: String, required: true },
-  filters: { type: Array as () => string[], required: true },
   activeFilters: { type: Array as () => string[], required: true }
 });
 
